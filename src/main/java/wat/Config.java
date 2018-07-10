aff80e2d9bd3b81e9710e64fb53dd8cf4a1dc788
Nick Holloway <nick.holloway@blackpepper.co.uk>
2018-07-10 09:38:57 +0100

    Remove items not related to bug reproduction
    - use latest pack-jvm-consumer-junit5 artifact
    - remove presence of junit-vintage
    - don't set request headers
    - don't duplicate test annotations
    - JUnit5 nested classes
    - make tests different

--- src/main/java/wat/Config.java
+++ src/main/java/wat/Config.java
@@ -1,16 +1,7 @@
 package wat;
 
-import static java.lang.String.format;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -21,16 +12,6 @@ public class Config {
   public RestTemplate getRestTemplate() {
     RestTemplate restTemplate = new RestTemplate();
     restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:8080"));
-    List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>();
-    interceptors.add(new ClientHttpRequestInterceptor() {
-      @Override
-      public ClientHttpResponse intercept(HttpRequest request, byte[] body,
-          ClientHttpRequestExecution execution) throws IOException {
-        request.getHeaders().add("Authorization", format("Bearer: %s", "abc"));
-        return execution.execute(request, body);
-      }
-    });
-    restTemplate.setInterceptors(interceptors);
     return restTemplate;
   }
 }
--- src/test/java/wat/FirstLevel.java
+++ src/test/java/wat/FirstLevel.java
@@ -3,16 +3,12 @@ package wat;
 import static java.util.Arrays.asList;
 
 import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.model.RequestResponsePact;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ExtendWith(PactConsumerTestExt.class)
 @SpringBootTest
+@PactTestFor(providerName = "upstream-service", port = "8080")
 class FirstLevel {
 
   @Autowired
@@ -29,83 +27,49 @@ class FirstLevel {
   @Autowired
   private ObjectMapper objectMapper;
 
-  @Nested
-  @SpringBootTest
-  class SecondLevel {
+  Long[] theData;
 
+  @Pact(consumer = "the-consumer", provider = "upstream-service")
+  public RequestResponsePact setupPact1(PactDslWithProvider builder)
+      throws JsonProcessingException {
 
-    @Nested
-    @SpringBootTest
-    @ExtendWith(PactConsumerTestExt.class)
-    @PactTestFor(providerName = "upstream-service", port = "8080")
-    class FirstActualTest {
+    theData = new Long[]{1L, 2L, 3L};
 
-      Long[] theData;
+    return builder
+        .uponReceiving("a put request with data")
+        .path("/first/1/third/fourth")
+        .body(objectMapper.writeValueAsString(theData))
+        .method("PUT")
+        .willRespondWith()
+        .status(204)
+        .toPact();
+  }
 
-      @Pact(consumer = "the-consumer", provider = "upstream-service")
-      public RequestResponsePact setupPact(PactDslWithProvider builder)
-          throws JsonProcessingException {
-        Map<String, String> requestHeaders = new HashMap<>();
+  @Test
+  @PactTestFor(pactMethod = "setupPact1")
+  public void returnsSuccessStatus1() {
+    sut.putSomeData(1, asList(theData));
+  }
 
-        Map<String, String> expectedResponseHeaders = new HashMap<>();
+  @Pact(consumer = "the-consumer", provider = "upstream-service")
+  public RequestResponsePact setupPact2(PactDslWithProvider builder)
+      throws JsonProcessingException {
 
-        theData = new Long[]{1L, 2L, 3L};
+    theData = new Long[]{4L, 5L, 6L};
 
-        return builder
-            .uponReceiving("a put request with data")
-            .path("/first/1/third/fourth")
-            .body(objectMapper.writeValueAsString(theData))
-            .headers(requestHeaders)
-            .method("PUT")
-            .willRespondWith()
-            .status(204)
-            .headers(expectedResponseHeaders)
-            .toPact();
-      }
+    return builder
+        .uponReceiving("another put request with data")
+        .path("/first/2/third/fourth")
+        .body(objectMapper.writeValueAsString(theData))
+        .method("PUT")
+        .willRespondWith()
+        .status(204)
+        .toPact();
+  }
 
-      @Test
-      @PactVerification()
-      public void returnsSuccessStatus() {
-        sut.putSomeData(1, asList(theData));
-      }
-    }
-
-
-    @Nested
-    @SpringBootTest
-    @ExtendWith(PactConsumerTestExt.class)
-    @PactTestFor(providerName = "upstream-service", port = "8080")
-    class SecondActualTest {
-
-      Long[] theData;
-
-      @Pact(consumer = "the-consumer", provider = "upstream-service")
-      public RequestResponsePact setupPact(PactDslWithProvider builder)
-          throws JsonProcessingException {
-        Map<String, String> requestHeaders = new HashMap<>();
-
-        Map<String, String> expectedResponseHeaders = new HashMap<>();
-
-        theData = new Long[]{1L, 2L, 3L};
-
-        return builder
-            .uponReceiving("another put request with data")
-            .path("/first/1/third/fourth")
-            .body(objectMapper.writeValueAsString(theData))
-            .headers(requestHeaders)
-            .method("PUT")
-            .willRespondWith()
-            .status(204)
-            .headers(expectedResponseHeaders)
-            .toPact();
-      }
-
-      @Test
-      @PactVerification()
-      public void returnsSuccessStatus() {
-        sut.putSomeData(1, asList(theData));
-      }
-    }
+  @Test
+  @PactTestFor(pactMethod = "setupPact2")
+  public void returnsSuccessStatus2() {
+    sut.putSomeData(2, asList(theData));
   }
 }
-
